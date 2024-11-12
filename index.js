3cb617afa575b4d777c2a68871e28c49e1ac9356
testgithubritik <161156122+testgithubritik@users.noreply.github.com>
2024-11-13 01:04:49 +0530

    Add files via upload
    js

---
+++ index.js
@@ -0,0 +1,70 @@
+//1
+/*let input  = 1;
+let M = 5050;
+function  findgrade(m) {
+    let grade  ;
+    if (m < 10){grade = 'E';}
+    else if  (m >=11 && m <=20){grade = 'D';}
+        else if (m >=21 && m <=30){grade = 'c';}
+        else if (m >=31 && m <=40){grade ='B';}
+        else if (m >=41 && m <=50){grade = 'A';}
+        else { grade ='invalid input';}
+    }
+    
+    console.log("the grade is  :  " + findgrade(input));*/
+    
+
+
+    //3
+
+     /*let a = 2;
+    let b = 5;
+    let c = 4;
+    function findLargest(a,b,c){
+        if(a<b && b<c   && c<a) {
+            console.log("all number are same");
+        }
+            else { 
+                let largest = Math.max(a,b,c);
+                console.log("the largest number is : " + largest);
+
+            }
+        }*/
+
+
+
+//5
+ /*function triangletype(a,b,c){
+    let sides =[a,b,c].sort((x,y)=>x-y);
+    let [x,y,z]= sides;
+    if (x**2 +y**2 > z**2){
+        return "acute triangle";
+    } else if (x**2 + y**2 < z**2){
+        return "obtuse triangle";
+    } else {
+        return "right triangle";
+    }
+ }
+console.log(triangletype(60,100,20));*/
+
+
+//4
+/*function secondsmallestnumber (x,y,z){
+    const arr = [2,9,23];
+    arr. sort((a,b)=>a-b);
+return arr [1];
+}
+console.log(secondsmallestnumber(2,9,23));*/
+
+
+//2
+/*const myobject = { 
+P : "PrepBytes ",
+ Z :"Zenith" ,
+ E :"Expert Coder", 
+ D :"Data Structure",
+};
+console.log(myobject.E);*/
+    
+
+
